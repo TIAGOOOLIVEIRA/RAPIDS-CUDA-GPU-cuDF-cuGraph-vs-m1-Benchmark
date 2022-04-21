7041b48362e5ed1244381c3a3c34cc593b6fa8a8
Tiago Oliveira <ti.olive@gmail.com>
2022-04-21 15:38:41 +0100

    adding new operations over cuGraph lib

--- loadingParquet_GPU_Accelerated.py
+++ loadingParquet_GPU_Accelerated.py
@@ -22,6 +22,11 @@ import cugraph
 import cudf, io
 from io import StringIO
  
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+#cuDF
+
 filepathparquet = '/tmp/datasets/fgroup.parquet.gzip'
  
 maxiter = 30000000
@@ -72,3 +77,76 @@ print(timedelta(seconds=end_time - start_time))
 #0:00:00.144295
  
 #same joining operation takes 4x slower in a CPU based Pandas Dataframe using same data scale.
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+#cuGraph
+
+#Patent citation network
+#https://snap.stanford.edu/data/cit-Patents.html
+#Nodes: 3774768
+#Edges: 16518948
+gfilepath = "/tmp/datasets/cit-Patents.txt"
+
+start_time = time.monotonic()
+gdf = cudf.read_csv(gfilepath, delimiter='\t', names=['src', 'dst'], dtype=['int32', 'int32'] )
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
+#HDD 0:00:01.898568
+
+--louvain
+gdf["data"] = 1.0
+
+G = cugraph.Graph()
+
+start_time = time.monotonic()
+G.from_cudf_edgelist(gdf, source='src', destination='dst', edge_attr='data')
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
+#HDD 0:00:00.523784
+
+#Run PageRank on the Graph
+start_time = time.monotonic()
+df_PR = cugraph.pagerank(G)
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
+#HDD 0:00:02.796918
+
+#Exploring some ranked nodes
+df_PR.sort_values(by='vertex', ascending=False, na_position='first')
+df_PR.sort_values(by='pagerank', ascending=False, na_position='first').tail(3)
+df_PR.loc[df_PR["vertex"] == 2836131]
+
+
+########################################################################################################################
+#Playing with others Graph algorithms
+
+#Louvain
+start_time = time.monotonic()
+df_L, mod = cugraph.louvain(G)
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
+#
+
+#Ensemble Clustering for Graphs (ECG)
+start_time = time.monotonic()
+df_ECG = cugraph.ecg(G)
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
+#
+
+part_ids = df_L["partition"].unique()
+print(str(len(part_ids)) + " partition detected")
+
+#Fetching nodes for one specific Louvain partition
+vids = df_L.query("partition == 1")
+v = cudf.Series(vids['vertex'])
+
+subG = cugraph.subgraph(G, v)
+print("\tNumber of Vertices: " + str(G.number_of_vertices()))
+print("\tNumber of Edges:    " + str(G.number_of_edges()))
+
+
+coo = cudf.DataFrame()
+subDF = subG.view_edge_list()
